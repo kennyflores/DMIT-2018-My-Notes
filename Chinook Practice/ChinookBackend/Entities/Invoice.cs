da1d7650cbcfc09adabfc41b16c118db5cdd54a5
Kenneth Flores <flores.ken21@gmail.com>
2018-09-11 00:47:15 -0600

    Addition of Entities
    - Customers
    - Invoices

---
+++ Chinook Practice/ChinookBackend/Entities/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookBackend.Entities
+{
+    [Table("Invoices")]
+    public class Invoice
+    {
+        [Key]
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public string BillingAddress { get; set; }
+        public string BillingCity { get; set; }
+        public string BillingState { get; set; }
+        public string BillingCountry{ get; set; }
+        public string BillingPostalCode { get; set; }
+        public decimal Total { get; set; }
+    }
+}
